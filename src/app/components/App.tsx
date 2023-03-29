cac3eea264da9199bb39d60bf4445d763b85de37
Ole Jørgen <olejorgenbakken@icloud.com>
2023-03-29 15:28:10 +0200

    added a recently updated section

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -6,39 +6,9 @@ import meta from "@navikt/aksel-icons/metadata";
 import { useState } from 'react';
 import { getFillIcon, categorizeIcons } from '../utils';
 import { useMemo } from 'react';
-import Fuse from 'fuse.js';
-
-const fuseStroke = new Fuse(
-  Object.values(meta).filter((x) => x.variant.toLowerCase() === "stroke"),
-  {
-    threshold: 0.2,
-    keys: [
-      { name: "name", weight: 3 },
-      { name: "category", weight: 2 },
-      { name: "sub_category", weight: 2 },
-      { name: "keywords", weight: 3 },
-      { name: "variant", weight: 1 },
-    ],
-    shouldSort: false,
-  }
-);
-
-const fuseFill = new Fuse(getFillIcon(Object.values(meta)), {
-  threshold: 0.2,
-  keys: [
-    { name: "name", weight: 3 },
-    { name: "category", weight: 2 },
-    { name: "sub_category", weight: 2 },
-    { name: "keywords", weight: 3 },
-    { name: "variant", weight: 1 },
-  ],
-  shouldSort: false,
-});
+import { Button } from '@navikt/ds-react';
 
 function App() {
-  const [query] = useState("");
-  const [toggle] = useState<"stroke" | "fill">("stroke");
-
   const [strokeIcons] = useState(
     Object.values(meta).filter((x) => x.variant.toLowerCase() === "stroke")
   );
@@ -46,21 +16,10 @@ function App() {
   const [fillIcons] = useState(getFillIcon(Object.values(meta)));
 
   const categories = useMemo(() => {
-    if (toggle === "fill") {
-      return categorizeIcons(
-        query
-          ? fuseFill.search(query).map((result) => result.item as any)
-          : fillIcons
-      );
-    }
     return categorizeIcons(
-      query
-        ? fuseStroke.search(query).map((result) => result.item as any)
-        : strokeIcons
+      strokeIcons
     );
-  }, [toggle, query, strokeIcons, fillIcons]);
-
-
+  }, [strokeIcons, fillIcons]);
 
   React.useEffect(() => {
     window.onmessage = (event) => {
@@ -71,66 +30,93 @@ function App() {
     };
   }, []);
 
+  const newIcons = () => {
+    let sortedIcons = strokeIcons.sort((a, b) => a.updated_at < b.updated_at ? 1 : -1);
+    return sortedIcons.slice(0, 10);
+  };
+
+  newIcons()
+
   return (
     <div className='plugin-wrapper'>
       <header className='header'>
-        <img src={logo} />
-        <h2>Aksel Icons</h2>
+        <img src={logo} className="logo" />
+        <h1 className='name'>Aksel Icons</h1>
       </header>
 
-      <div className='icons'>
-        {categories.map((cat) => {
-          return (
-            <div key={cat.category} className="category">
-              <h2>
-                {cat.category}
-              </h2>
-              {cat.sub_categories.map((sub) => {
-                return (
-                  <div key={sub.sub_category} className="sub-category">
-                    <h3>
-                      {sub.sub_category}
-                    </h3>
-                    <div className='icon-grid'>
-                      {sub.icons.map((i) => {
-                        const T = Icons[`${i.id}Icon`];
-                        if (T === undefined) {
-                          return null;
-                        }
-                        const iconSVG = React.useRef<SVGElement>(undefined);
+      <div className='collection updated'>
+        <h2 className='title'>Recently updated</h2>
+        <div className='icon-grid'>
+          {newIcons().map((i) => {
+            const T = Icons[`${i.id}Icon`];
+            if (T === undefined) {
+              return null;
+            }
+            const iconSVG = React.useRef<SVGElement>(undefined);
 
-                        const onCreate = (name: string) => {
-                          let svg = iconSVG.current.outerHTML;
-                          let size = 24;
-                          parent.postMessage({ pluginMessage: { type: 'create-icon', svg, size, name } }, '*');
-                        };
+            const onCreate = (name: string) => {
+              let svg = iconSVG.current.outerHTML;
+              let size = 24;
+              parent.postMessage({ pluginMessage: { type: 'create-icon', svg, size, name } }, '*');
+            };
 
-                        return (
-                          <div key={i.id} className='icon' onClick={() => {
-                            onCreate(i.name);
-                          }}>
-                            <T
-                              className="text-3xl"
-                              aria-hidden
-                              alt=""
-                              fontSize="1.5rem"
-                              ref={iconSVG}
-                            />
-                            <span className='name'>
-                              {i.name}
-                            </span>
-                          </div>
-                        );
-                      })}
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
+            return (
+              <Button key={i.id} variant="tertiary-neutral" icon={<T
+                className="text-3xl"
+                aria-hidden
+                alt=""
+                fontSize="1.5rem"
+                ref={iconSVG}
+              />} onClick={() => {
+                onCreate(i.name);
+              }} />
+            );
+          })
+          }
+        </div>
       </div>
-    </div >
+
+      {categories.map((cat) => {
+        return (
+          <div key={cat.category} className="collection all">
+            {cat.sub_categories.map((sub) => {
+              return (
+                <div key={sub.sub_category} className="category">
+                  <h2 className='title'>{cat.category}&nbsp;<span className='sub-category'>| {sub.sub_category}</span></h2>
+                  <div className='icon-grid'>
+                    {sub.icons.map((i) => {
+                      const T = Icons[`${i.id}Icon`];
+                      if (T === undefined) {
+                        return null;
+                      }
+                      const iconSVG = React.useRef<SVGElement>(undefined);
+
+                      const onCreate = (name: string) => {
+                        let svg = iconSVG.current.outerHTML;
+                        let size = 24;
+                        parent.postMessage({ pluginMessage: { type: 'create-icon', svg, size, name } }, '*');
+                      };
+
+                      return (
+                        <Button key={i.id} variant="tertiary-neutral" icon={<T
+                          className="text-3xl"
+                          aria-hidden
+                          alt=""
+                          fontSize="1.5rem"
+                          ref={iconSVG}
+                        />} onClick={() => {
+                          onCreate(i.name);
+                        }} />
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
